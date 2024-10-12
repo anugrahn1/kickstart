041e87e59172a412199d4caf12fd5f5c937e71a5
Anugrah <pnanugrah@gmail.com>
2024-10-11 23:36:20 -0700

    removed neorg

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -38,29 +38,6 @@ return {
   },
 
   {
-    'nvim-neorg/neorg',
-    -- lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    event = 'VeryLazy',
-    version = '*', -- Pin Neorg to the latest stable release
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {},
-          ['core.concealer'] = {},
-          ['core.dirman'] = {
-            config = {
-              workspaces = {
-                notes = '~/notes/',
-              },
-              default_workspace = 'notes',
-            },
-          },
-        },
-      }
-    end,
-  },
-
-  {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
